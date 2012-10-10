548b67c173b890f1e6b108e24517c67ddf05a5d3
Tom Harman <tom@harmantom.com>
2012-10-10 11:45:29 -0400

    Added the video link and updated copy & small style.

--- index.php
+++ index.php
@@ -15,42 +15,49 @@
     <head>
       
         <meta charset="utf-8" />
-        <title>Drop UI – A Super Quick iPhone Prototyping tool for Designers</title>
-        
+        <title>Drop UI – A Super-Quick iPhone Prototyping tool for Designers</title>
         
         <!-- Our CSS stylesheet file -->
         <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800,300' rel='stylesheet' type='text/css'>
         <link href='http://fonts.googleapis.com/css?family=Gentium+Book+Basic:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="assets/css/styles.css" />
-        
-        
+
         <!--[if lt IE 9]>
           <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
+        
+        <script type="text/javascript">
+
+          // if user is accessing page from an iPhone then do this...
+          if (window.navigator.userAgent.indexOf('iPhone') != -1) {
+        		if (window.navigator.standalone == true) {
+        		  // app running from iPhone Homescreen
+
+        		}else{
+        		  // app running from iPhone Safari
+        		  document.write('<h1 style="font-size: 7em; padding: 1.5em 1em">DropUI is super-quick iPhone prototyping tool for designers. <i>A computer is required to get started.</i></h1><style type="text/css">div#container { display: none; }</style>');
+        		}
+        	}
+        </script>
     </head>
     
     <body>
-      
-      <? if(isset($_GET['id'])){ ?>
-
-      <h1>Please load this link from your iPhone</h1>
-
-      <? } else { ?>
       
       <div id="container">
 		
   		<header>
   			<h1>A super-quick iPhone prototyping tool for designers.</h1>
-  			<p><!-- Useful for remote research and demonstrating user journeys. --> To get started, simply drag your UI comps onto the iPhone screen on the left. After uploading, these screens will be turned into a tappable iPhone webapp, accessible at the URL below. Be careful not to refresh the page, or you'll have to start again.</p>
+  			<p><!-- Useful for remote research and demonstrating user journeys. --> Simply drag your UI comps onto the iPhone screen on the left. After uploading, these screens will be turned into a tappable iPhone webapp, "downloadable" at the URL below.</p>
     		<div id="url">
     		  <p><a href="<?= $ROOT_URL?><?= $_SESSION['public_id'] ?>"><?= $ROOT_URL?><?= $_SESSION['public_id'] ?></a></p>
           <!-- maybe add email functionality -->
     		</div>
+    		<p id="demo-video">Also, here's a <a href="https://vimeo.com/51004204">demonstration video</a>.</p>
     		<div id="credits">
-    		  <p>A small project by <a href="http://twitter.com/tomharman">@tomharman</a> with help from <a href="www.teehanlax.com/blog/iphone-gui-psd-v4/">Teehan + Lax's iPhone PSD</a> and <a href="http://tutorialzine.com/2011/09/html5-file-upload-jquery-php/">this HTML5 upload tutorial</a>. Source on <a href="https://github.com/tomharman/dropUI">Github</a>.</p>
+    		  <p>A tiny project by <a href="http://twitter.com/tomharman">@tomharman</a> with help from <a href="www.teehanlax.com/blog/iphone-gui-psd-v4/">Teehan + Lax</a> &amp; <a href="http://tutorialzine.com/2011/09/html5-file-upload-jquery-php/">Tutorial Zine</a>. Source on <a href="https://github.com/tomharman/dropUI">Github</a>. <em>Refreshing this page will start over.</em></p>
     		</div>
   		</header>
-	  <!-- <img src="assets/img/iPhone.png" width="403" height="868" alt="IPhone" /> -->
+  		
 	    <div id="iphone-frame">
 	      <? date_default_timezone_set('America/New_York') ?>
 	      <p id="iphone-time"><?= date('h:i')?></p>
@@ -60,34 +67,15 @@
   		</div>
 
 		</div>
-      
-      <? } ?>
-      <!-- Including The jQuery Library -->
-  		<script src="http://code.jquery.com/jquery-1.6.3.min.js"></script>
-	
-  		<!-- Including the HTML5 Uploader plugin -->
-  		<script src="assets/js/jquery.filedrop.js"></script>
-	
-  		<!-- The main script file -->
-      <script src="assets/js/script.js"></script>
-      
-      <script type="text/javascript">
-        
-        // if user is accessing page from an iPhone then do this...
-        if (window.navigator.userAgent.indexOf('iPhone') != -1) {
-      		if (window.navigator.standalone == true) {
-      		  // app running from iPhone Homescreen
-            
-      		}else{
-      		  // app running from iPhone Safari
-      		  <? if(isset($_GET['id'])){ ?>
-      		    // no id specified, throw an error or say you need to add images from a computer
-      		  <? } ?>
-        		  document.location.href = '<?=$ROOT_URL?>i/<?= $_GET["id"]; ?>';
-        		  // - show generic instructions on how to download 'app' to homescreen, iPhone optimized
-      		}
-      	}
-      </script>
+
+    <!-- Including The jQuery Library -->
+		<script src="http://code.jquery.com/jquery-1.6.3.min.js"></script>
+
+		<!-- Including the HTML5 Uploader plugin -->
+		<script src="assets/js/jquery.filedrop.js"></script>
+
+		<!-- The main script file -->
+    <script src="assets/js/script.js"></script>
 
     </body>
 </html>
--- iphone.php
+++ iphone.php
@@ -1,19 +1,21 @@
 <? require_once('global.php'); ?>
+<? if(isset($_GET['id'])){ $dir  = getcwd() . '/uploads/' . $_GET['id'] . '/'; } ?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
     
     <meta charset="utf-8" />
-    <title>Drop UI</title>
+    <title>Drop UI – A Super-Quick iPhone Prototyping tool for Designers</title>
 
     <meta name="viewport" content="width=device-width, user-scalable=no, height=device-height">
-    <link rel="apple-touch-icon-precomposed" href="assets/img/iosicon.png">
-    <link rel="apple-touch-startup-image" href="assets/img/iosicon.png">
-    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="none" />
   
     <base href="<?=$ROOT_URL?>" />
   
+    <link rel="apple-touch-icon-precomposed" href="assets/img/iosicon.png" />
+    <link rel="apple-touch-startup-image" href="assets/img/iosicon.png" />
     <link rel="stylesheet" href="assets/css/styles.css" />
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800,300' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Gentium+Book+Basic:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
@@ -23,21 +25,26 @@
         window.location.href = theLink.href;
       }
       
-      <? if(!isset($_GET['page']) && isset($_GET['id'])){?>
+      <? if(!isset($_GET['page']) && isset($_GET['id'])) { ?>
       if (window.navigator.userAgent.indexOf('iPhone') != -1) {
     		if (window.navigator.standalone == true) {
     			//initialize();
-    			document.location.href = '<?=$ROOT_URL?>i/<?= $_GET["id"]; ?>/0';
+    			document.location.href = '<?=$ROOT_URL?><?= $_GET["id"]; ?>/0';
     		}else{
+    			<? if(file_exists($dir)) { ?>
     			document.write('<h1>Tap the share button below and select<br /> <i>Add to <br />Home Screen</i></h1>');
+    			<? } else { ?>
+    			document.write('<h1><i>DropUI</i><br />ID not found, please check and try again.</h1>');
+    			<? } ?>
     		}
     	}else{
         // document.location.href = 'please-open-from-iphone.html';
+        document.write('<h1>Please load this link from your iPhone</h1>');
     	}
     	<? } ?>
       // prevent user scrolling in app
       function blockMove() {
-            event.preventDefault() ;
+        event.preventDefault() ;
       }
     
     </script>
@@ -52,9 +59,7 @@
   
   <? } elseif(isset($_GET['page'])) {
   
-  $dir  = getcwd() . '/uploads/' . $_GET['id'] . '/';
-  $files = scandir($dir);
-  
+  $files = scandir($dir);  
   // removes first, blank to records from array
   array_splice($files, 0, 2);
   
@@ -64,9 +69,9 @@
   <div id="overlay">
     <nav>
       <? if($_GET['page']!=0){?>
-      <a id="overlay-previous" href="<?=$ROOT_URL?>i/<?= $_GET["id"]; ?>/<?= $_GET['page']-1?>" onclick="OpenLink(this); return false">previous screen</a>
+      <a id="overlay-previous" href="<?=$ROOT_URL?><?= $_GET["id"]; ?>/<?= $_GET['page']-1?>" onclick="OpenLink(this); return false">previous screen</a>
       <? } if($_GET['page']+1!=count($files)) { ?>
-      <a id="overlay-next" href="<?=$ROOT_URL?>i/<?= $_GET["id"]; ?>/<?= $_GET['page']+1?>" onclick="OpenLink(this); return false">next screen</a>
+      <a id="overlay-next" href="<?=$ROOT_URL?><?= $_GET["id"]; ?>/<?= $_GET['page']+1?>" onclick="OpenLink(this); return false">next screen</a>
       <? } ?>
     </nav>
     <div id="meta">
